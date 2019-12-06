32f627a1b093d6943791594e866dbdc8d4a69675
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-06 17:57:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,10 @@ end
 introduction("Sam")
 
 
-
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language("Sam", "Ruby")
 
 
 
